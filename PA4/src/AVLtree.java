b162906d1bd7256cbaa9901827495539bf12d0f7
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2017-03-28 10:39:45 -0400

    Updated AVLtree to work as a node.

--- PA4/src/AVLtree.java
+++ PA4/src/AVLtree.java
@@ -1,35 +1,17 @@
 
 public class AVLtree<K extends Comparable<? super K>, D> {
-	private final static int ALLOWED_IMBALANCE = 1;
-	private AVLNode root;
-	private int size;
-	private int sizeOfRightSubtree;
 
-	public AVLtree() {
-		root = null;
-		size = 0;
-		sizeOfRightSubtree = 0;
-	}
-	
-	
+	public K element;
+	public int height;
+	// public AVLNode parent;
+	public AVLtree<K,D> left;
+	public AVLtree<K,D> right;
 
-	public void insert(K k) {
-		root = insert(k, root);
-	}
-
-	private class AVLNode {
-		public int element;	// int instead of generic since insert and remove specify ints	
-		public int height;
-//		public AVLNode parent;
-		public AVLNode left;
-		public AVLNode right;
-		
-		public AVLNode(int element){ 
-			this.element = element;
-			this.height = 0;
-			left = null;
-			right = null;
-		}	
+	public AVLtree(K element) {
+		this.element = element;
+		this.height = 0;
+		left = null;
+		right = null;
 	}
 
 }
